179ecdc34799dcb7296278a49fb92035f27c3d83
Thomas Hendry <thomas.hendry@hva.nl>
2026-02-04 10:57:01 +0100

    add game tile positioning

--- scripts.js
+++ scripts.js
@@ -1,18 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+  positionGameTiles();
+});
+
 addEventListener("keydown", (e) => {
-    const marker = document.querySelector(".marker");
-    if (marker === null) return;
-    const x = Number(window.getComputedStyle(marker).gridColumn);
-    const y = Number(window.getComputedStyle(marker).gridRow);
-    if (e.key == "d") {
-        marker.style.gridColumn = x+1;
-    }
-    if (e.key == "a") {
-        marker.style.gridColumn = x-1;
-    }
-    if (e.key == "s") {
-        marker.style.gridRow = y+1;
-    }
-    if (e.key == "w") {
-        marker.style.gridRow = y-1;
-    }
-})
+  tryMovePlayer(e);
+});
+
+const navButtons = [];
+
+function positionGameTiles() {
+  const game = document.querySelector("#game");
+  if (game === null) return;
+
+  for (const tile of game.children) {
+    const placeX = tile.getAttribute("data-x");
+    const placeY = tile.getAttribute("data-y");
+    tile.style.gridColumn = placeX;
+    tile.style.gridRow    = placeY;
+  }
+}
+
+function tryMovePlayer(e) {
+  const player = document.querySelector("#player");
+  if (player === null) return;
+
+  const x = Number(window.getComputedStyle(player).gridColumn);
+  const y = Number(window.getComputedStyle(player).gridRow);
+  switch (e.key) {
+    case "d":
+      player.style.gridColumn = x + 1;
+      break;
+    case "a":
+      player.style.gridColumn = x - 1;
+      break;
+    case "s":
+      player.style.gridRow = y + 1;
+      break;
+    case "w":
+      player.style.gridRow = y - 1;
+      break;
+  }
+}
